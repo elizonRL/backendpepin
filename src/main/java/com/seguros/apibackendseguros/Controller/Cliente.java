4d102d34a924d1fb7ba5dec5a383e4b8eee22a30
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2023-11-25 03:36:17 -0400

    first commit

---
+++ src/main/java/com/seguros/apibackendseguros/Controller/Cliente.java
@@ -0,0 +1,31 @@
+package com.seguros.apibackendseguros.Controller;
+
+import com.seguros.apibackendseguros.models.PolizaModels;
+import com.seguros.apibackendseguros.service.PolizaServise;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/poliza")
+public class Cliente {
+
+    @Autowired
+    private PolizaServise polizaServise;
+    @PersistenceContext
+    private EntityManager em ;
+
+    @CrossOrigin(origins = "http://localhost:63342")
+    @GetMapping
+    public ArrayList<PolizaModels> obtenerClientes(@RequestParam String num_poliza){
+
+        return (ArrayList<PolizaModels>) this.polizaServise.getPolizas(num_poliza);
+    }
+
+}
